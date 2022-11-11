c2206858cb030daafa174c1273f57898df6b8762
Joana Camacho <joana.camacho@appdetex.com>
2022-11-11 11:42:06 +0000

    clearing main

--- src/main/java/com/appdetex/repository/MarketplaceDetectionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.appdetex.repository;
-
-import com.appdetex.entity.MarketplaceDetection;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface MarketplaceDetectionRepository extends JpaRepository<MarketplaceDetection, Long> {
-}
