54fe3e56d2ba24f5441c9f15f4ba2bd977f88a1d
Pedro Reis <pedro.reis@appdetex.com>
2022-11-28 11:27:10 +0000

    commiting to avoid errors while pulling

--- src/main/java/com/appdetex/controller/AuditController.java
+++ src/main/java/com/appdetex/controller/AuditController.java
@@ -13,6 +13,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/audit/")
 public class AuditController {
     @Autowired
--- src/main/java/com/appdetex/controller/MarketplaceDetectionController.java
+++ src/main/java/com/appdetex/controller/MarketplaceDetectionController.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @RestController
+@CrossOrigin
 @RequestMapping("/api/marketplacedetections/")
 public class MarketplaceDetectionController {
 
