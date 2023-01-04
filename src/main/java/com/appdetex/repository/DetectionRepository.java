4e2a68114652772016142ea420c628d2ae0780cf
Pedro Reis <pedro.reis@appdetex.com>
2023-01-04 16:03:32 +0000

    Adds query that returns the day and the number of detections in that day

--- src/main/java/com/appdetex/repository/DetectionRepository.java
+++ src/main/java/com/appdetex/repository/DetectionRepository.java
@@ -2,8 +2,10 @@ package com.appdetex.repository;
 
 import com.appdetex.entity.Detection;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
 import java.util.List;
 
 
@@ -12,4 +14,6 @@ public interface DetectionRepository extends JpaRepository<Detection, Integer> {
 
     List<Detection> findByAccountId(int accountId);
     Detection findById(int id);
+    @Query("SELECT SUBSTRING(captureDate,1,10), COUNT(*) FROM Detection GROUP BY 1")
+    ArrayList<String> getNumberDetectionsByDay();
 }
