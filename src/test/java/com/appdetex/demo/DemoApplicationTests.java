26ac652860a5bc55c367b5d15f1cd893c533f9c0
Pedro Temtem <pedro.temtem@appdetex.com>
2023-01-04 10:17:27 +0000

    more changes and tests for rules started

--- src/test/java/com/appdetex/demo/DemoApplicationTests.java
+++ src/test/java/com/appdetex/demo/DemoApplicationTests.java
@@ -1,7 +1,11 @@
 package com.appdetex.demo;
 
+import com.appdetex.controller.*;
+import com.appdetex.repository.AuditRepository;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
@@ -9,8 +13,32 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 @SpringBootTest
 class DemoApplicationTests {
 
+	@Autowired
+	AccountController accountController;
+
+	@Autowired
+	AuditController auditController;
+
+	@Autowired
+	BrandTrackController brandTrackController;
+
+	@Autowired
+	DetectionController detectionController;
+
+	@Autowired
+	RoleController roleController;
+
+	@Autowired
+	UserController userController;
+
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
+		Assertions.assertThat(auditController).isNotNull();
+	}
+
+	@Test
+	public void contextLoads2(){
+		Assertions.assertThat(accountController).isNotNull();
 	}
 
 }
---
+++ src/test/java/com/appdetex/rulesengine/RuleJacuzziBrandTest.java
@@ -0,0 +1,15 @@
+package com.appdetex.rulesengine;
+
+import com.appdetex.entity.Detection;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class RuleJacuzziBrandTest {
+
+
+    @Test
+    void rulesChecker() {
+    }
+}
