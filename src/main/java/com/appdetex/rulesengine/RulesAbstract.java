0cbd03a8cf1c709a3534ecb1aee9cb678ad301a1
Joana Camacho <joana.camacho@appdetex.com>
2023-01-09 10:33:44 +0000

    making json construction more object oriented and refactoring rules engine

--- src/main/java/com/appdetex/rulesengine/RulesAbstract.java
+++
@@ -1,5 +0,0 @@
-package com.appdetex.rulesengine;
-
-public abstract class RulesAbstract {
-
-}
--- src/main/java/com/appdetex/rulesengine/RulesInterface.java
+++
@@ -1,9 +0,0 @@
-package com.appdetex.rulesengine;
-
-import com.appdetex.entity.Detection;
-
-public interface RulesInterface {
-
-    Detection rulesChecker(Detection detection);
-
-}
