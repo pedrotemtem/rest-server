991c1be47b9ab173aa19d8ecb9fadb313e1aafb6
Pedro Reis <pedro.reis@appdetex.com>
2023-01-12 09:25:02 +0000

    adds metric table / class

--- src/main/java/com/appdetex/entity/MetricPkId.java
+++ src/main/java/com/appdetex/entity/MetricPkId.java
@@ -1,7 +1,6 @@
 package com.appdetex.entity;
 
 import lombok.AllArgsConstructor;
-import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
@@ -10,7 +9,6 @@ import java.io.Serializable;
 @AllArgsConstructor
 @NoArgsConstructor
 @EqualsAndHashCode
-@Data
 public class MetricPkId implements Serializable {
     private String harvestProcess;
     private String marketplace;
